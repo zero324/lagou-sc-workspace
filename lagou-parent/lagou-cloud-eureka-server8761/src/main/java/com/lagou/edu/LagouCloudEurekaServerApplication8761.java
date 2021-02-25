b3a870f747cfc804c4b1de129ca22282b2e2695c
zero <272880214@qq.com>
2021-02-26 00:48:12 +0800

    eureka 集群   客户端注册到服务端

---
+++ lagou-parent/lagou-cloud-eureka-server8761/src/main/java/com/lagou/edu/LagouCloudEurekaServerApplication8761.java
@@ -0,0 +1,14 @@
+package com.lagou.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//声明本项目是一个eureka server服务
+@EnableEurekaServer
+public class LagouCloudEurekaServerApplication8761 {
+    public static void main(String[] args) {
+        SpringApplication.run(LagouCloudEurekaServerApplication8761.class,args);
+    }
+}
