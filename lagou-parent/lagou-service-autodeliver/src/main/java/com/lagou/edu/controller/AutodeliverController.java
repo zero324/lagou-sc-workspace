c2be66779b382f5cef9be0115fec2cea998960dc
zero <272880214@qq.com>
2021-02-28 15:34:14 +0800

    服务消费端  从注册中心中获取服务列表  调用 服务端接口

--- lagou-parent/lagou-service-autodeliver/src/main/java/com/lagou/edu/controller/AutodeliverController.java
+++ lagou-parent/lagou-service-autodeliver/src/main/java/com/lagou/edu/controller/AutodeliverController.java
@@ -1,11 +1,14 @@
 package com.lagou.edu.controller;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/autodeliver")
@@ -13,9 +16,27 @@ public class AutodeliverController {
     @Resource
     private RestTemplate restTemplate;
 
-    @RequestMapping("/resumestate/{userId}")
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+  /*  @RequestMapping("/resumestate/{userId}")
     public Integer resumestate(@PathVariable Long userId){
         Integer forObject = restTemplate.getForObject("http://localhost:8080/resume/getState/" + userId, Integer.class);
         return  forObject;
+    }*/
+
+    @RequestMapping("/resumestate/{userId}")
+    public Integer resumestate(@PathVariable Long userId) {
+        //discoveryClient  客户端获取 服务列表
+        List<ServiceInstance> instances = discoveryClient.getInstances("lagou-server-resume");
+        //从服务列表中选一个服务(负载均衡)
+        ServiceInstance serviceInstance = instances.get(0);
+        //获取服务信息的元数据
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        String url ="http://"+host + ":" +port +"/resume/getState/" + userId;
+        System.out.println("从eureka server集群中获取 服务信息拼接的url "+url);
+        Integer forObject = restTemplate.getForObject(url, Integer.class);
+        return forObject;
     }
 }
---
+++ lagou-parent/lagou-service-autodeliver/src/test/java/AutodelliverApplicationTest.java
@@ -0,0 +1,25 @@
+import com.lagou.edu.AutodeliverApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest(classes = {AutodeliverApplication.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class AutodelliverApplicationTest {
+    @Resource
+    private DiscoveryClient discoveryClient;
+    @Test
+    public void metadatatest(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("lagou-server-resume");
+        //从服务列表中选一个服务(负载均衡)
+        ServiceInstance serviceInstance = instances.get(0);
+        System.out.println("获取自定义的元数据========================>"+serviceInstance.getMetadata().get("cluster"));
+    }
+}
