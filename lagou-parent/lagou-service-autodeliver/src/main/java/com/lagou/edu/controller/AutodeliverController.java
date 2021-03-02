c2feab57ca90c3c6e325f65e66caa5f837c487a5
zero <272880214@qq.com>
2021-03-03 01:01:19 +0800

    服务消费端 hystrix熔断器配置

--- lagou-parent/lagou-service-autodeliver/src/main/java/com/lagou/edu/controller/AutodeliverController.java
+++ lagou-parent/lagou-service-autodeliver/src/main/java/com/lagou/edu/controller/AutodeliverController.java
@@ -44,35 +44,72 @@ public class AutodeliverController {
 
     /**
      * ribbon 做负载均衡
+     *
      * @param userId
      * @return
      */
     @RequestMapping("/resumestate/{userId}")
-    public Integer resumestate(@PathVariable Long userId){
+    public Integer resumestate(@PathVariable Long userId) {
         //利用ribbon做负载均衡  在注册testtemplate方法上加@LoadBalance
-        String url="http://lagou-server-resume/resume/getState/" + userId;
+        String url = "http://lagou-server-resume/resume/getState/" + userId;
         Integer forObject = restTemplate.getForObject(url, Integer.class);
-        return  forObject;
+        return forObject;
     }
 
     /**
      * 提供者模拟处理超时   服务消费端 断路器设置
+     *
      * @param userId
      * @return
      */
     // 使⽤@HystrixCommand注解进⾏熔断控制
-   @HystrixCommand(
+    @HystrixCommand(
             // commandProperties熔断的⼀些细节属性配置
             commandProperties = {
                     //一个属性一个@HystrixProperty注解  属性在hystrixcommandproperties类里
-                    @HystrixProperty(name ="execution.isolation.thread.timeoutInMilliseconds",value="2000")//默认是1000
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")//默认是1000
             }
     )
     @RequestMapping("/resumestatetimeout/{userId}")
-    public Integer resumestatetimeout(@PathVariable Long userId){
+    public Integer resumestatetimeout(@PathVariable Long userId) {
         //利用ribbon做负载均衡  在注册testtemplate方法上加@LoadBalance
-        String url="http://lagou-server-resume/resume/getState/" + userId;
+        String url = "http://lagou-server-resume/resume/getState/" + userId;
         Integer forObject = restTemplate.getForObject(url, Integer.class);
-        return  forObject;
+        return forObject;
+    }
+
+    /**
+     * 提供者模拟处理超时   服务消费端 断路器设置
+     *
+     * @param userId
+     * @return
+     */
+    // 使⽤@HystrixCommand注解进⾏熔断控制
+    @HystrixCommand(
+            // commandProperties熔断的⼀些细节属性配置
+            commandProperties = {
+                    //一个属性一个@HystrixProperty注解  属性在hystrixcommandproperties类里
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")//默认是1000
+            }, fallbackMethod = "myfailback" // 回退⽅法
+    )
+    @RequestMapping("/resumestatetimeoutfailback/{userId}")
+    public Integer resumestatetimeoutfailback(@PathVariable Long userId) {
+        //利用ribbon做负载均衡  在注册testtemplate方法上加@LoadBalance
+        String url = "http://lagou-server-resume/resume/getState/" + userId;
+        Integer forObject = restTemplate.getForObject(url, Integer.class);
+        return forObject;
+    }
+
+    /*
+    定义回退⽅法，返回预设默认值
+    注意**
+    降级（兜底）⽅法必须和被降级⽅法相同的⽅法签名（相同参数列表、相同返回值）
+    可以在类上使⽤@DefaultProperties注解统⼀指定整个类中共⽤的降级（兜底）⽅法
+    服务提供者端（简历微服务）模拟请求超时（线程休眠3s），只修改8080实例，8081不修改，对
+    ⽐观察
+    注意：该⽅法形参和返回值与原始⽅法保持⼀致
+    */
+    public Integer myfailback(Long userid) {
+        return -1;
     }
 }
