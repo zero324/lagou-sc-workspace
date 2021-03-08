bb3dcb7958078cd08455075ea6ff19855e1cfd6b
fuwei <272880214@qq.com>
2021-03-08 13:59:26 +0800

    config client 手动更新

---
+++ lagou-parent/lagou-cloud-autodeliver-feign-8092/src/main/java/com/lagou/edu/controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.lagou.edu.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RefreshScope
+@RestController
+@RequestMapping("/configclient")
+public class ConfigClientController {
+    @Value("${lagou.message}")
+    private String message;
+
+    @RequestMapping("viewconfig")
+    public String viewConfig(){
+        return "lagou.message===>"+message;
+    }
+
+}
