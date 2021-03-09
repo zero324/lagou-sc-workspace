9adab86adfa9e107f89cc591d378618f0136d4da
zero <272880214@qq.com>
2021-03-10 01:12:12 +0800

    消息驱动组件stream 增加自定义channel

---
+++ lagou-parent/lagou-cloud-stream-producer-9090/src/main/java/com/lagou/edu/service/customizechannel/CustomChannel.java
@@ -0,0 +1,8 @@
+package com.lagou.edu.service.customizechannel;
+
+/**
+ * 因为bindings 里有input output这两个channel只能有一个同一个类型的chanel 所以需要自定义channel
+ * 自定义channel 模仿接口Source.class,Sink.class 并且使用也是类似
+ */
+public interface CustomChannel {
+}
