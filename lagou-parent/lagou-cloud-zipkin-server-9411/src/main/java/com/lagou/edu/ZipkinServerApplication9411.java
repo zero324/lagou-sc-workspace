e43025c9e19bcd622adc99d3843ef8809c2cfc61
zero <272880214@qq.com>
2021-03-14 21:32:39 +0800

    增加zipkin server 的持久化配置

--- lagou-parent/lagou-cloud-zipkin-server-9411/src/main/java/com/lagou/edu/ZipkinServerApplication9411.java
+++ lagou-parent/lagou-cloud-zipkin-server-9411/src/main/java/com/lagou/edu/ZipkinServerApplication9411.java
@@ -2,12 +2,26 @@ package com.lagou.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
 import zipkin2.server.internal.EnableZipkinServer;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableZipkinServer//开启zipkinServer
 public class ZipkinServerApplication9411 {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication9411.class,args);
+    }
+
+    //注入事务管理
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
     }
 }
