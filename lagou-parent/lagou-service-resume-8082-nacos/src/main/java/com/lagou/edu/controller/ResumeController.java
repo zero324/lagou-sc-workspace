73dcc07179ba62bbefcf2d0969edf0a348d8c515
fuwei <272880214@qq.com>
2021-03-17 18:00:19 +0800

    nacos 配置中心

--- lagou-parent/lagou-service-resume-8082-nacos/src/main/java/com/lagou/edu/controller/ResumeController.java
+++ lagou-parent/lagou-service-resume-8082-nacos/src/main/java/com/lagou/edu/controller/ResumeController.java
@@ -4,6 +4,7 @@ import com.lagou.edu.pojo.Resume;
 import com.lagou.edu.service.ResumeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/resume")
+@RefreshScope
 public class ResumeController {
     @Autowired
     private ResumeService resumeService;
 
     @Value("${server.port}")
     private Integer port;
+    @Value("${lagou.message}")
+    private Integer message;
     @GetMapping("/getState/{userId}")
     public Integer getDefaultResumeByUserId(@PathVariable Long userId){
         System.out.println("调用了lagou-service-resume-8082");
+        System.out.println("配置中心配置lagou.message"+message);
        //return resumeService.findDefaultResumeByUserId(userId).getIsOpenResume();
        return port;
     }
