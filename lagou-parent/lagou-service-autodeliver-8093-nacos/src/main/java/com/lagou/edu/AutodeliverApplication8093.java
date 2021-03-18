bcaaffdba86a6f82cf45c0eaa178fdd52a497b83
fuwei <272880214@qq.com>
2021-03-18 18:27:46 +0800

    sentinel 配置

--- lagou-parent/lagou-service-autodeliver-8093-nacos/src/main/java/com/lagou/edu/AutodeliverApplication8093.java
+++ lagou-parent/lagou-service-autodeliver-8093-nacos/src/main/java/com/lagou/edu/AutodeliverApplication8093.java
@@ -1,14 +1,10 @@
 package com.lagou.edu;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,19 +25,6 @@ public class AutodeliverApplication8093 {
         return new RestTemplate();
     }
 
-    /*
-        在被监测的微服务中注册监控servlet（⾃动投递微服务，监控数据就是来⾃于这个微服务）前提的有actuator jar
-     */
-    @Bean
-    public ServletRegistrationBean getServlet() {
-        HystrixMetricsStreamServlet streamServlet = new
-                HystrixMetricsStreamServlet();
-        ServletRegistrationBean registrationBean = new
-                ServletRegistrationBean(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/actuator/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
-    }
+
 
 }
--- lagou-parent/lagou-service-autodeliver-8093-nacos/src/main/java/com/lagou/edu/controller/AutodeliverController.java
+++ lagou-parent/lagou-service-autodeliver-8093-nacos/src/main/java/com/lagou/edu/controller/AutodeliverController.java
@@ -1,7 +1,5 @@
 package com.lagou.edu.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,26 +17,6 @@ public class AutodeliverController {
     @Resource
     private DiscoveryClient discoveryClient;
 
-  /*  @RequestMapping("/resumestate/{userId}")
-    public Integer resumestate(@PathVariable Long userId){
-        Integer forObject = restTemplate.getForObject("http://localhost:8080/resume/getState/" + userId, Integer.class);
-        return  forObject;
-    }*/
-
-  /*  @RequestMapping("/resumestate/{userId}")
-    public Integer resumestate(@PathVariable Long userId) {
-        //discoveryClient  客户端获取 服务列表
-        List<ServiceInstance> instances = discoveryClient.getInstances("lagou-server-resume");
-        //从服务列表中选一个服务(负载均衡)
-        ServiceInstance serviceInstance = instances.get(0);
-        //获取服务信息的元数据
-        String host = serviceInstance.getHost();
-        int port = serviceInstance.getPort();
-        String url ="http://"+host + ":" +port +"/resume/getState/" + userId;
-        System.out.println("从eureka server集群中获取 服务信息拼接的url "+url);
-        Integer forObject = restTemplate.getForObject(url, Integer.class);
-        return forObject;
-    }*/
 
     /**
      * ribbon 做负载均衡
@@ -59,27 +37,7 @@ public class AutodeliverController {
         方法上加一个@HystricCommand注解相当于一个熔断器
      */
 
-    /**
-     * 提供者模拟处理超时   服务消费端 断路器设置
-     *
-     * @param userId
-     * @return
-     */
-    // 使⽤@HystrixCommand注解进⾏熔断控制
-    @HystrixCommand(
-            // 线程池标识，要保持唯⼀，不唯⼀的话就共⽤了
-            threadPoolKey = "findResumeOpenStateTimeout",
-            // 线程池细节属性配置
-            threadPoolProperties = {
-                    @HystrixProperty(name="coreSize",value = "1"), // 线程数
-                    @HystrixProperty(name="maxQueueSize",value="20") // 等待队列⻓度
-            },
-            // commandProperties熔断的⼀些细节属性配置
-            commandProperties = {
-                    //一个属性一个@HystrixProperty注解  属性在hystrixcommandproperties类里
-                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")//默认是1000
-            },fallbackMethod = "myfailback" // 回退⽅法
-    )
+
     @RequestMapping("/resumestatetimeout/{userId}")
     public Integer resumestatetimeout(@PathVariable Long userId) {
         //利用ribbon做负载均衡  在注册testtemplate方法上加@LoadBalance
@@ -88,41 +46,7 @@ public class AutodeliverController {
         return forObject;
     }
 
-    /**
-     * 提供者模拟处理超时   服务消费端 断路器设置
-     *
-     * @param userId
-     * @return
-     */
-    // 使⽤@HystrixCommand注解进⾏熔断控制
-    @HystrixCommand(
-            // 线程池标识，要保持唯⼀，不唯⼀的话就共⽤了
-            threadPoolKey = "resumestatetimeoutfailback",
-            // 线程池细节属性配置
-            threadPoolProperties = {
-                    @HystrixProperty(name="coreSize",value = "2"), // 线程数
-                    @HystrixProperty(name="maxQueueSize",value="20") // 等待队列⻓度
-            },
-            // commandProperties熔断的⼀些细节属性配置
-            commandProperties = {
-                    //一个属性一个@HystrixProperty注解  属性在hystrixcommandproperties类里
-                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")//默认是1000
-                    //hystrix高级配置 ,指定hystrix流程细节配置
-                    //设置hystrix流程的时间窗口
-                    ,@HystrixProperty(name =
-                            "metrics.rollingStats.timeInMilliseconds",value = "8000"),
-                    //最小请求书
-                    @HystrixProperty(name =
-                            "circuitBreaker.requestVolumeThreshold",value = "2"),
-                    //失败率阀值 50%
-                    @HystrixProperty(name =
-                            "circuitBreaker.errorThresholdPercentage",value = "50"),
-                    //活动窗口  每隔3秒 放一个请求访问
-                    @HystrixProperty(name =
-                            "circuitBreaker.sleepWindowInMilliseconds",value = "3000")
 
-            }, fallbackMethod = "myfailback" // 回退⽅法
-    )
     @RequestMapping("/resumestatetimeoutfailback/{userId}")
     public Integer resumestatetimeoutfailback(@PathVariable Long userId) {
         //利用ribbon做负载均衡  在注册testtemplate方法上加@LoadBalance
