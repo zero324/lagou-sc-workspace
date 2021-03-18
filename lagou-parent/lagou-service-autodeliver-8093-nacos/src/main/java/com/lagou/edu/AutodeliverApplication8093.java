118b8c2c058e6ed0142a941b7f3611799282c885
zero <272880214@qq.com>
2021-03-19 00:28:45 +0800

    调用resume模块

--- lagou-parent/lagou-service-autodeliver-8093-nacos/src/main/java/com/lagou/edu/AutodeliverApplication8093.java
+++ lagou-parent/lagou-service-autodeliver-8093-nacos/src/main/java/com/lagou/edu/AutodeliverApplication8093.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 //开启注册中心服务端
 @EnableDiscoveryClient
 //@EnableHystrix  开启hystrix熔断器
-@EnableCircuitBreaker  //开启熔断器
+//@EnableCircuitBreaker  //开启熔断器
 //@SpringCloudApplication 是 @SpringBootApplication @EnableDiscoveryClient @EnableCircuitBreaker 组合注解
 public class AutodeliverApplication8093 {
     public static void main(String[] args) {
